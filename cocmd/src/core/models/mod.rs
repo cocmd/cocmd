4ce93d67cf181953331258db0ef1b0c42722908b
Your Name <mosher@mosher.com>
2023-09-15 13:38:27 +0300

    removing unneeded stuff

--- cocmd/src/core/models/mod.rs
+++ cocmd/src/core/models/mod.rs
@@ -1,3 +1,4 @@
+pub mod cmd_exit;
 pub mod script_model;
 pub mod settings;
 pub mod source_config_model;
--- cocmd/src/lib.rs
+++ cocmd/src/lib.rs
@@ -2,10 +2,8 @@
 // #![warn(missing_docs)] // uncomment for docs
 
 pub mod core;
-mod data;
-mod runner;
 pub mod utils;
+
 pub use self::core::consts;
+pub use self::core::models::cmd_exit::CmdExit;
 pub use self::core::models::settings::Settings;
-pub use self::runner::run;
-pub use data::{CmdExit, CMD};
