a4ced97e5bf218710f6acb75518884ae8ac93c99
Your Name <you@example.com>
2023-10-03 20:44:29 +0300

    linux

--- .github/scripts/ubuntu/build_deb.sh
+++ .github/scripts/ubuntu/build_deb.sh
@@ -8,16 +8,16 @@ cargo install cargo-deb --version 1.34.0
 cd cocmd
 
 echo "Building X11 deb package"
-cargo deb -p cocmd -- --no-default-features
+cargo deb -p cocmd 
 
-echo "Building Wayland deb package"
-cargo deb -p cocmd --variant wayland -- --no-default-features
+# echo "Building Wayland deb package"
+# cargo deb -p cocmd --variant wayland 
 
 cd ..
 cp cocmd/target/debian/cocmd_*.deb cocmd-debian-x11-amd64.deb
 sha256sum cocmd-debian-x11-amd64.deb > cocmd-debian-x11-amd64-sha256.txt
-cp cocmd/target/debian/cocmd-wayland*.deb cocmd-debian-wayland-amd64.deb
-sha256sum cocmd-debian-wayland-amd64.deb > cocmd-debian-wayland-amd64-sha256.txt
+# cp cocmd/target/debian/cocmd-wayland*.deb cocmd-debian-wayland-amd64.deb
+# sha256sum cocmd-debian-wayland-amd64.deb > cocmd-debian-wayland-amd64-sha256.txt
 ls -la
 
 echo "Copying to mounted volume"
