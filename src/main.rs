01c10ea56f47312077177697317f740af4559dcc
Your Name <you@example.com>
2023-10-08 00:09:00 +0300

    Bump version to 1.0.53

--- src/main.rs
+++ src/main.rs
@@ -31,7 +31,7 @@ use crate::output::set_tracing;
 #[derive(Parser)]
 #[command(
     author = "Moshe Roth",
-    version = "1.0.52",
+    version = "1.0.53",
     about = "
     Cocmd is a CLI utility to collaborate on anything in the CMD in the community and internal teams. 
     Use it to sync Aliases, Scripts, and Workflows."
